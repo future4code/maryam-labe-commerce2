14d18f16a9e53ddb786b07a182c11b9beb185e0d
Fabio Faria <fabiofadev87@gmail.com>
2021-09-16 10:48:47 -0300

    Corrigido padronizacao

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
 import "./App.css";
+import Filtro from "./components/Filtro";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="filtros-container">
-        <h3>Filtros</h3>
-        <label htmlFor="valor minimo">
-          Valor minimo:
-          <input type="number" min="0" />
-        </label>
-        <label htmlFor="valor maximo">
-          Valor maximo:
-          <input type="number" min="0" />
-        </label>
-        <label htmlFor="busca por nome">
-          Busca por nome
-          <input type="text" placeholder="Produto" />
-        </label>
-      </div>
+      <>
+        <Filtro />
+      </>
     );
   }
 }
