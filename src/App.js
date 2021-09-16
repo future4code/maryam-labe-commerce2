60d7a4dfdc99dc5a1248b3f8f5eaa3572c75dfd7
Fabio Faria <fabiofadev87@gmail.com>
2021-09-16 11:10:44 -0300

    Corrigido conflitos e arrumando os padroes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
-import { Products } from './components/Products/Products';
+import React from "react";
+import { Products } from "./components/Products/Products";
 import styled from "styled-components";
-
+import "./styles/Produtos.css";
 
 const AppContainer = styled.div`
   display: grid;
@@ -11,53 +10,50 @@ const AppContainer = styled.div`
   gap: 8px;
 `;
 
-
 const products = [
   {
     id: 1,
-    name: 'Nave Espacial Blaster Version',
+    name: "Nave Espacial Blaster Version",
     price: 10000,
-    photo: 'https://i.ibb.co/18ZyWFX/img1.png'
+    photo: "https://i.ibb.co/18ZyWFX/img1.png",
   },
   {
     id: 2,
-    name: 'Foguete Top das Galáxias',
+    name: "Foguete Top das Galáxias",
     price: 18000,
-    photo: 'https://i.ibb.co/ryxNYvD/topdasgalaxias.png'
+    photo: "https://i.ibb.co/ryxNYvD/topdasgalaxias.png",
   },
   {
     id: 3,
-    name: 'Ônibus Espacial #AdeusTerra',
+    name: "Ônibus Espacial #AdeusTerra",
     price: 30000,
-    photo: 'https://i.ibb.co/5LXV1Kn/adeus-terra.png'
-  },  
+    photo: "https://i.ibb.co/5LXV1Kn/adeus-terra.png",
+  },
   {
     id: 4,
-    name: 'Foguete na Planta -Previsão de Entrega: 2025',
+    name: "Foguete na Planta -Previsão de Entrega: 2025",
     price: 7000,
-    photo: 'https://i.ibb.co/zs2m3Vn/naplanta.png'
+    photo: "https://i.ibb.co/zs2m3Vn/naplanta.png",
   },
   {
     id: 5,
-    name: 'Foguete Nasa Só Para Baixinhos',
+    name: "Foguete Nasa Só Para Baixinhos",
     price: 4000,
-    photo: 'https://i.ibb.co/6wp2fyn/sopara.png'
+    photo: "https://i.ibb.co/6wp2fyn/sopara.png",
   },
   {
     id: 6,
-    name: 'Nave Espacial Missão McFly - Back to 1985',
+    name: "Nave Espacial Missão McFly - Back to 1985",
     price: 100000,
-    photo: 'https://i.ibb.co/DkM9nts/mcfly.png'
-  }
-]
+    photo: "https://i.ibb.co/DkM9nts/mcfly.png",
+  },
+];
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <AppContainer>
-        <Products 
-          products={products}
-        />
+        <Products products={products} />
       </AppContainer>
     );
   }
