42ccfee8acdf7c4ab2e8ff4e6bad1d5f95782f82
canavarrojacqueline <canavarrojacqueline@gmail.com>
2021-09-19 14:32:20 -0300

    Ajuste na tela com os filtros e carrinho compras

--- src/App.js
+++ src/App.js
@@ -1,107 +1,152 @@
-import React from "react";
-import "./components/styles/App.css";
-import { Products } from "./components/Products";
+import React from 'react';
+import { Filters } from './components/Filters/Filters';
+import { Products } from './components/Products/Products';
+import { ShoppingCart } from './components/ShoppingCart/ShoppingCart';
+import styled from 'styled-components';
 
-import Filtro from "./components/Filtro";
-import Carrinho from "./components/Carrinho";
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`;
+
+const products = [
+  {
+    id: 1,
+    name: 'Produto Espacial 1',
+    price: 3000,
+    photo: "https://i.ibb.co/87x58pV/adeus-terra.jpg"
+  },
+  {
+    id: 2,
+    name: 'Produto Espacial 2',
+    price: 1000,
+    photo: "https://i.ibb.co/rbPdqHn/img1.jpg"
+  },
+  {
+    id: 3,
+    name: 'Produto Espacial 3',
+    price: 18000,
+    photo: "https://i.ibb.co/wzkt7LL/topdasgalaxias.jpg"
+  },
+  {
+    id: 4,
+    name: 'Produto Espacial 4',
+    price: 7000,
+    photo: "https://i.ibb.co/ypDL4rk/mcfly.jpg"
+  },
+  {
+    id: 5,
+    name: 'Produto Espacial 5',
+    price: 4000,
+    photo: "https://i.ibb.co/pnQDTwz/naplanta.jpg"
+  },
+  {
+    id: 6,
+    name: 'Produto Espacial 6',
+    price: 10000,
+    photo: "https://i.ibb.co/QmrpF3P/sopara.jpg"
+  }
+]
 
 class App extends React.Component {
   state = {
-    produtos: [
+    minFilter: 100,
+    maxFilter: 1000,
+    nameFilter: 'Produto',
+    productsInCart: [
       {
-        id: 1,
-        name: "Nave Espacial Blaster Version",
-        price: 1000,
-        photo: "https://i.ibb.co/18ZyWFX/img1.png",
-      },
-      {
-        id: 2,
-        name: "Foguete Top das Galáxias",
-        price: 18000,
-        photo: "https://i.ibb.co/ryxNYvD/topdasgalaxias.png",
+        id: 4,
+        name: 'Produto Espacial 4',
+        price: 7000,
+        photo: "https://i.ibb.co/ypDL4rk/mcfly.jpg",
+        quantity: 1
       },
       {
         id: 3,
-        name: "Ônibus Espacial #AdeusTerra",
-        price: 3000,
-        photo: "https://i.ibb.co/5LXV1Kn/adeus-terra.png",
-      },
-      {
-        id: 4,
-        name: "Foguete na Planta -Previsão de Entrega: 2025",
-        price: 7000,
-        photo: "https://i.ibb.co/zs2m3Vn/naplanta.png",
-      },
-      {
-        id: 5,
-        name: "Foguete Nasa Só Para Baixinhos",
-        price: 4000,
-        photo: "https://i.ibb.co/6wp2fyn/sopara.png",
-      },
-      {
-        id: 6,
-        name: "Nave Espacial Missão McFly - Back to 1985",
-        price: 1000,
-        photo: "https://i.ibb.co/DkM9nts/mcfly.png",
-      },
-    ],
-    maxFilter: "",
-    minFilter: "",
-    filtroNome: "",
-    listaCarrinho: [],
-    valorTotal: 0,
-  };
-  filtrarValorMin = (e) => {
-    this.setState({ minFilter: e.target.value });
-  };
-  filtrarValorMax = (e) => {
-    this.setState({ maxFilter: e.target.value });
-  };
+        name: 'Produto Espacial 3',
+        price: 18000,
+        photo: "https://i.ibb.co/wzkt7LL/topdasgalaxias.jpg",
+        quantity: 2
+      }
+    ]
+  }
 
-  filtrarPeloNome = (e) => {
-    this.setState({ filtroNome: e.target.value });
-  };
+  onChangeMinFilter = (event) => {
+    this.setState({minFilter: event.target.value})
+  }
 
-  adicionarProdutoCarrinho = (produto) => {
-    const listaCarrinho = [...this.state.listaCarrinho];
-    listaCarrinho.push(produto);
-    this.setState({ listaCarrinho });
-  };
+  onChangeMaxFilter = (event) => {
+    this.setState({maxFilter: event.target.value})
+  }
 
-  renderizarItensNoCarrinho = () => {};
+  onChangeNameFilter = (event) => {
+    this.setState({nameFilter: event.target.value})
+  }
 
-  removerItensCarrinho = (produto) => {
-    alert(produto);
-  };
+  onAddProductToCart = (productId) => {
+    const productInCart = this.state.productsInCart.find(product => productId === product.id)
+
+    if(productInCart) {
+      const newProductsInCart = this.state.productsInCart.map(product => {
+        if(productId === product.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1
+          }
+        }
+
+        return product
+      })
+
+      this.setState({productsInCart: newProductsInCart})
+    } else {
+      const productToAdd = products.find(product => productId === product.id)
+
+      const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1}]
+
+      this.setState({productsInCart: newProductsInCart})
+    }
+  }
+
+  onRemoveProductFromCart = (productId) => {
+    const newProductsInCart = this.state.productsInCart.map((product) => {
+      if(product.id === productId) {
+        return {
+          ...product,
+          quantity: product.quantity - 1
+        }
+      }
+      return product
+    }).filter((product) => product.quantity > 0)
+
+    this.setState({productsInCart: newProductsInCart})
+  }
 
   render() {
     return (
-      <div className="app-container">
-        <Filtro
-          filtrarMin={this.filtrarValorMin}
-          filtrarMax={this.filtrarValorMax}
-          filtroMin={this.state.minFilter}
-          filtroMax={this.state.maxFilter}
-          filtroNome={this.state.filtroNome}
-          filtrarNome={this.filtrarPeloNome}
+      <AppContainer>
+        <Filters
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onChangeMinFilter={this.onChangeMinFilter}            
+          onChangeMaxFilter={this.onChangeMaxFilter}            
+          onChangeNameFilter={this.onChangeNameFilter}                  
         />
-        <div className="produtos-container">
-          <Products
-            buscaMin={this.state.minFilter}
-            buscaMax={this.state.maxFilter}
-            buscaNome={this.state.filtroNome}
-            products={this.state.produtos}
-            adicionarCarrinho={this.adicionarProdutoCarrinho}
-          />
-        </div>
-        <Carrinho
-          nome={this.state.listaCarrinho.name}
-          itensDoCarrinho={this.state.listaCarrinho}
-          valorTotal={this.state.valorTotal}
-          quantidade={this.state.listaCarrinho.length}
-          removerItens={this.removerItensCarrinho}
+        <Products 
+          products={products}
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onAddProductToCart={this.onAddProductToCart}
         />
-      </div>
+        <ShoppingCart
+          productsInCart={this.state.productsInCart}
+          onRemoveProductFromCart={this.onRemoveProductFromCart}
+        />
+      </AppContainer>
     );
   }
 }
